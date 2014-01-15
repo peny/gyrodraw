36460e697f96974a577bbcd859a87cc9fd782af6
Petter <petter@pttr.se>
2014-01-15 15:17:39 +0000

    added gyrodraw

---
+++ app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+*/
+
+var express = require('express');
+var io = require('socket.io').listen(3334);
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('mousemove', function (data) {
+    io.sockets.emit('mousemove',data);
+  });
+});
+
+
+app.listen(3333);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ public/javascripts/helper.js
@@ -0,0 +1,30 @@
+var socket = io.connect('http://50.56.121.17:3334');
+var can;
+var ctx;
+function toHex(d){
+ return ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+}
+var rgb = '#'+toHex(Math.random()*256)+toHex(Math.random()*256)+toHex(Math.random()*256);
+
+$(document).ready(function(){
+  can = document.getElementById('can');
+  ctx = can.getContext('2d');
+
+  $(document).mousemove(function(e){
+    socket.emit('mousemove', {x: e.pageX, y: e.pageY,rgb: rgb});
+  });
+  var oldgamma = 100;
+  var oldbeta = 100;
+  window.addEventListener('deviceorientation', function(e) {
+    if(Math.abs(oldgamma-e.gamma) > 0.1 && Math.abs(oldbeta-e.beta) > 0.1){
+      oldgamma = e.gamma;
+      oldbeta = e.beta;
+      socket.emit('mousemove', {x: (e.gamma+180)*1.3, y: (e.beta+180)*1.3, rgb: rgb});
+    }
+  },false);
+  
+  socket.on('mousemove', function(data) {
+    ctx.setFillColor(data.rgb);
+    ctx.fillRect(data.x-60,data.y-60,10,10);
+  });
+});
