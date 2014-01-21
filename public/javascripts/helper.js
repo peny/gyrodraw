c2f24f662b4e1f1578307d5447d3e01367fe1e14
Petter <petter@pttr.se>
2014-01-21 18:30:22 +0100

    Update helper.js

--- public/javascripts/helper.js
+++ public/javascripts/helper.js
@@ -1,11 +1,17 @@
 window.scrollTo(0, 1);
 var socket = io.connect('http://50.56.121.17:3334');
+var SCREEN_OFFSET = 180;
+
 var can;
 var ctx;
+
 function toHex(d){
   return ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
 }
-var rgb = '#'+toHex(Math.random()*256)+toHex(Math.random()*256)+toHex(Math.random()*256);
+function getRandomRGBString(){
+  return '#'+toHex(Math.random()*256)+toHex(Math.random()*256)+toHex(Math.random()*256);
+}
+var rgb = getRandomRGBString();
 
 $(document).ready(function(){
   can = document.getElementById('can');
@@ -20,12 +26,12 @@ $(document).ready(function(){
     if(Math.abs(oldgamma-e.gamma) > 0.1 && Math.abs(oldbeta-e.beta) > 0.1){
       oldgamma = e.gamma;
       oldbeta = e.beta;
-      socket.emit('mousemove', {x: (e.gamma+180)*1.3, y: (e.beta+180)*1.3, rgb: rgb});
+      socket.emit('mousemove', {x: (e.gamma+SCREEN_OFFSET)*1.3, y: (e.beta+SCREEN_OFFSET)*1.3, rgb: rgb});
     }
   },false);
 
   socket.on('mousemove', function(data) {
     ctx.setFillColor(data.rgb);
-    ctx.fillRect(data.x-60,data.y-60,10,10);
+    ctx.fillRect(data.x-SCREEN_OFFSET/2,data.y-SCREEN_OFFSET/2,10,10);
   });
 });
